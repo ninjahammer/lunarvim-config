27cc09947b5e796b2556bbefd31bdc5cf7a39414
ninjahammer <abhishek.surbhat@gmail.com>
2025-06-22 20:53:32 +0530

    added alias

--- alias.lua
+++ alias.lua
@@ -1,5 +1,5 @@
 
 lvim.keys.insert_mode["jk"] = "<Esc>"
 lvim.keys.normal_mode["<C-d>"] = "<C-d>zz"
-lvim.keys.normal_mode["gt"] = ":NvimTreeFocus<Cr>"
+lvim.keys.normal_mode["gt"] = ":NvimTreeFocus<CR>"
 lvim.keys.normal_mode["bl"] = ":GitBlameToggle<CR>"
